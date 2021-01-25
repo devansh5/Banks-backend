14b01eef5d7d5ecd3f0c0307b989707e87142574
devansh5 <devkumar5436@gmail.com>
2021-01-25 13:37:32 +0530

    adding district and address

--- bankAPI/views.py
+++ bankAPI/views.py
@@ -5,17 +5,18 @@ from .serializers import BranchSerializer
 
 # Create your views here.
 
-
+# search across all fields
 class BranchViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    search_fields= ['branch','city','ifsc','state','bank__name']
+    search_fields= ['branch','city','district','address','ifsc','state','bank__name']
     ordering_fields=['ifsc']
     filter_backends=[filters.SearchFilter,filters.OrderingFilter]
     queryset = Branches.objects.all()
     serializer_class = BranchSerializer
 
+# autocomplete api for searching over branches
 class BranchAutoViewSet(viewsets.ModelViewSet):
     search_fields=['branch',]
     filter_backends=(filters.SearchFilter,)
